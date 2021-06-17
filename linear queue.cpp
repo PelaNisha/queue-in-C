e8dc4c115a4c7f2e65d3f2f870172bb7ec578fe9
Nisha pela <84078164+PelaNisha@users.noreply.github.com>
2021-06-17 18:36:33 +0545

    Update linear queue.cpp

--- linear queue.cpp
+++ linear queue.cpp
@@ -1,121 +1,145 @@
-// array implementation for linear queue
+ // array implementation for linear queue
 
 #include<stdio.h>
-#include<stdlib.h>
 
 #define SIZE 4
 
+//initializing global variables
 int lqueue[SIZE];
 int front = -1, rear = -1;
 
+//declaring functions
 void insert(int);
 void display();
 bool isEmpty();
 bool isFull();
 int peek();
-void del();
+int del();
 
+//insert function
 void insert(int item)
 {
-	if (isFull() == true)
-		printf("STack is full\n");
-	else if(isEmpty() == true)
+	if (isFull())//check full condition
 	{
-			front=0; rear=0;
-			lqueue[rear]=item;
+			printf("Queue is full\n");
+	}else if(isEmpty())
+	{
+			front=0; rear=0;// initialize rear and front to 0
+			lqueue[rear]=item;//pass item to the rear position
 	}
 	else{
 			
-			lqueue[++rear]=item;	
+			lqueue[++rear]=item;	// else increment rear and pass item to rear
 }}
 
-void del()
+//delete function
+int del()
 {
 	int item;
-	if(isEmpty()==true)
-		printf("Empty");
-	else if(rear==front)
+	if(isEmpty())//check empty
+	{
+			printf("Empty\n");
+	}else if(rear==front)
 		{
-				item = lqueue[front];
-			rear == front ==-1;
+			item = lqueue[front];//pass the front to item and make queue empty
+			rear =-1; 
+			front=-1;
 		}
 	else
-		item = lqueue[front];
-		front++;	
+	{
+		item = lqueue[front];		//pass the element at the front to item
+		front++;					//and increment front
+
+	}
+	printf("Deleted item: %d\n",item);
+		
 }
 
+//empty function
 bool isEmpty()
 {
-	return rear==-1 && front==-1;
+	return (rear==-1 && front==-1);//return true if rear=front=-1
 }
+
+//full function
 bool isFull()
 {
-	return rear== SIZE-1;
+	return rear== SIZE-1;//return true if rear = size-1 
 }
+
+//peek function
 int peek()
 {
-	if(isEmpty()==true)
+	if(isEmpty())//check empty
 		printf("Empty, Nothing to peek\n");
 	else
-		printf("%d\n",lqueue[front]);
-}
-void display()
-{
-//	int temp = front;
-	if(isEmpty()==true)
 	{
-		printf("Nothing to display\n");
-	}
-	else
-		
-		for(int i = front; i<=rear; i++)
-		{
-				printf("%d | \n",lqueue[i]);
+			printf("%d\n",lqueue[front]);//print the first element
 	}
 }
 
+//diaplay function
+void display()
+{
+	if(isEmpty())//check empty
+	{
+		printf("Nothing to display!\n");
+	}
+	else
+	{
+			
+		for(int i = front; i<=rear; i++)
+		{
+				printf("%d | \n",lqueue[i]);//print every element from front to rear
+		}
+	}
+}
+
+//main function
 int main()
 {
-	int a, p =1, v;
-//	printf("%d\n", rear);
-//	printf("%d\n", lqueue[front]);
-//	printf("%d\n", lqueue[front]);
-	while(p!=0)
+	int a, v;
+	while(a!=7)
 	{
-	printf("Enter a option\n1.insert\n2.delete\n3.check empty\n4.check full\n5.display\n6.peek\n7.exit\n");
-	scanf("%d", &a);
-	switch(a)
-	{
-		case(1):
-			printf("Enter the value: ");
-			scanf("%d", &v);
-			insert(v);
-			break;
-		case(2):
-			del();
-			break;
-		case(3):
-			if(isEmpty()==true)
-				printf("Empty\n");
-			else printf("Not Empty\n");	
-			break;
-		case(4):
-			if(isFull()==true)
-				printf("Full\n");
-			else printf("Not full\n");
-			break;
-		case(5):
-			display();
-			break;
-		case(6):
-			peek();
-			break;	
-		case(7):
-			p--;
-			break;
-		default:
-			printf("Enter a valid input\n");
-			break;						
+		printf("Enter an option\n1.insert\n2.delete\n3.check empty\n4.check full\n5.display\n6.peek\n7.exit\n");
+		scanf("%d", &a);
+		switch(a)
+		{
+			case(1):
+				printf("Enter the value: ");
+				scanf("%d", &v);
+				insert(v);						//call insert function
+				break;
+			case(2):
+				del();
+				break;
+			case(3):
+				if(isEmpty())					//call empty function
+				{
+						printf("Empty\n");
+				}else{
+				 printf("Not Empty\n");	
+				}
+				break;
+			case(4):
+				if(isFull())					//call full function
+				{
+						printf("Full\n");
+				}else 
+				{
+					printf("Not full\n");
+				}break;
+			case(5):
+				display();						//call display function
+				break;
+			case(6):
+				peek();							//call peek function
+				break;	
+			case(7):
+				break;
+			default:
+				printf("Enter a valid input\n");//default output
+				break;						
 		}
 	}
 }
